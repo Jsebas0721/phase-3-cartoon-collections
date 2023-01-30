271f46c3ba4ab5111fc0787d247d0777288398d6
sebastian medina <sebastianlopez_21@hotmail.com>
2023-01-30 10:34:35 -0500

    commit tree e989be606c69f25e02d2e8cf11dd9ff288b370b2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(array_names)# code an argument here
   # Your code here
+  array_names.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array_calls)# code an argument here
   # Your code here
+  capitalized_calls = array_calls.map do |call|
+    call.capitalize << "!"
+  end
+  return capitalized_calls
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array_calls)# code an argument here
   # Your code here
+  array_calls.any? {|call| call.length > 4} 
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found_cheese = array_cheeses.find do |cheese|
+     cheese_types.include?(cheese) 
+  end
+  
 end
+
+find_the_cheese(["tomato soup", "oyster crackers", "gouda"])
+long_planeteer_calls(["two", "go", "industrious", "bop"])
